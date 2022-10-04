d2ec2525c92e5172321cd4dec19e11aa22fe2ffb
joaquin matus <joakike04gmail.com>
2022-10-04 09:51:14 -0600

    Se agregó el archivo expresiones2.cpp

---
+++ expresiones2.cpp
@@ -0,0 +1,39 @@
+/*
+Nombre del archivo: expresiones2.cpp
+autor: joacon
+lugar: itv
+instrucciones: Mas ejemplos con expresiones
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+	/* se le pedira al usuario que ingrese 2 numero (num1 = 5 , num2= 8)
+	el porgrama va a cambiar de posicion esos 2 valores
+	y se los va a mostrar al usuario (num1 = 8 y num2 = 5
+	*/
+	 double num1, num2, temp;
+	 cout<< "Ingrese el primer valor que desea agregar a: ";
+	 cin>> num1;
+	 
+	 cout<< "Ingrese el  segundo valor que desea agregar a: ";
+	 cin>> num2;
+	 
+	cout<< "*******************************************************" << endl;
+	
+	temp = num1;
+	num1 = num2;
+	num2 = temp;
+	
+	cout<< 
+	
+
+	
+	
+	 
+	 
+	return 0;
+}
