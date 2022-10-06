346dc5cf28774614517a86fd36ab234793e6a924
joaquin matus <joakike04gmail.com>
2022-10-06 08:45:43 -0600

    se añadio un nuevo archivo calculo salario neto

---
+++ calculosalarioneto.cpp
@@ -0,0 +1,15 @@
+/*
+Nombre del archivo: sumadenumero.cpp
+autor: joacon
+lugar: itv
+instrucciones: esto es solo una plantilla
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	return 0;
+}
+	
