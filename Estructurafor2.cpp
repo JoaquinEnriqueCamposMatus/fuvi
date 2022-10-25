0ab67124598a5133a12f3f6487803d3212b1bbd2
joaquin matus <joakike04gmail.com>
2022-10-25 12:54:57 -0600

    Se agg nueva carpeta

---
+++ Estructurafor2.cpp
@@ -0,0 +1,45 @@
+/*
+Nombre del archivo: Estrucutrafor1.cpp
+autor: joacon
+lugar: itv
+instrucciones: estructra for
+*/
+
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main(){
+int num,aleatorio,cont = 0, contimpar = 0, cant, temp, esEncontrado;
+	cout << "ingrese un numero: ";
+	cin >> num;
+	cout << "Cantidad de numeros: ";
+	cin >> cant;
+	srand (time(nullptr));
+	for (int i = 1; i <= cant; i++){
+		
+			aleatorio = rand () % 100;
+			cout << aleatorio << " ";
+		
+						if (aleatorio == num){
+							esEncontrado = true
+							temp = aleatorio
+			
+				
+		
+			
+			}
+			}
+
+	
+		cout << "fin del programa";
+		
+	
+	return 0;
+
+}
+
+	
+	
+	
